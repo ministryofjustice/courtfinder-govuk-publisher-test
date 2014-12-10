1d1ff1d531bd38b4654f12ae906cf763281953b4
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-12-10 17:50:03 +0000

    serialize court without pk or model fields

--- court/views.py
+++ court/views.py
@@ -6,20 +6,27 @@ from court.models import Court
 from django.http import HttpResponse
 import json
 
+
 def __failed_auth(request):
     return not hasattr(settings, 'OAUTH_TOKEN') or (request.META.get('HTTP_AUTHORIZATION','') != 'Bearer ' + settings.OAUTH_TOKEN)
+
+def __serialize_court(court):
+    return {'name':court.name}
 
 def list(request):
     if __failed_auth(request):
         return HttpResponseForbidden()
     courts = Court.objects.all()
-    return HttpResponse(serializers.serialize("json", courts))
+    court_objs = [__serialize_court(court) for court in courts]
+    return HttpResponse(json.dumps(court_objs))
 
 def court(request, uuid):
     if __failed_auth(request):
         return HttpResponseForbidden()
     if request.method == "GET":
-        return HttpResponse(get_object_or_404(court, uuid=uuid))
+        court = get_object_or_404(Court,uuid=uuid)
+        court_obj = __serialize_court(court)
+        return HttpResponse(json.dumps(court_obj))
 
     elif request.method == "PUT":
         c = json.loads(request.body);
@@ -54,8 +61,11 @@ def court(request, uuid):
             )
             return HttpResponse(status=201)
     elif request.method == "DELETE":
-        court = Court.objects.filter(uuid=uuid)
-        court.delete()
+        if uuid == 'all-the-things':
+            Court.objects.all().delete()
+        else:
+            court = Court.objects.filter(uuid=uuid)
+            court.delete()
         return HttpResponse(status=200)
     else:
         return HttpResponse(status=405)
