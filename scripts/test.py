c5285f5103dfd8e47b941c5990116378182e9237
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-12-11 12:29:29 +0000

    useful messages

--- scripts/test.py
+++ scripts/test.py
@@ -6,6 +6,7 @@ import urllib2
 
 base_url='http://127.0.0.1:8000/court/'
 oauth_token='foobar'
+num_errors=0
 
 #with open('../data/sample_court.json') as f:
 #    sample_court_json = f.read()
@@ -19,20 +20,25 @@ def same_arrays(a,b):
     else:
         return True
 
-def check(response, status_code, body=None, win='.'):
+def fail(message):
+    num_errors = num_errors + 1
+    print "FAIL: "+str(message)
+
+def check(description, response, status_code, body=None, win='WIN'):
+    sys.stdout.write(description+"...")
     if response['status_code'] != status_code:
-        print "Different status codes: expected %d, got %d" % (status_code, response['status_code'])
+        fail("Different status codes: expected %d, got %d" % (status_code, response['status_code']))
     elif body:
         b = json.loads(body)
         r = json.loads(response['text'])
         if type(b) == type([]) and not same_arrays(r,b):
-            print "Different arrays: expected %s, got %s" % (body, response['text'])
+            fail("FAIL: Different arrays: expected %s, got %s" % (body, response['text']))
         elif r != b:
-            print "Different objects: expected %s, got %s" % (body, response['text'])
+            fail("FAIL: Different objects: expected %s, got %s" % (body, response['text']))
         else:
-            sys.stdout.write(win)
+            print win
     else:
-        sys.stdout.write(win)
+        print win
 
 def http(method='GET', uuid='', auth=False, body=None):
     opener = urllib2.build_opener(urllib2.HTTPHandler)
@@ -60,17 +66,17 @@ def delete(uuid, auth=True):
 
 
 if __name__ == "__main__":
-    check(delete('all-the-things'), 200)
-    check(put('foo-bar', '[]', auth=False), 403)
-    check(list(), 200, '[]')
-    check(get('de305d54-75b4-431b-adb2-eb6b9e546013'), 404)
-    check(put('de305d54-75b4-431b-adb2-eb6b9e546013', sample_court_json_1), 201)
-    check(put('de305d54-75b4-431b-adb2-eb6b9e546013', 'bad json'), 400)
-    check(put('de305d54-75b4-431b-adb2-eb6b9e546013', sample_court_json_1), 200)
-    check(list(), 200, '['+sample_court_json_1+']')
-    check(get('de305d54-75b4-431b-adb2-eb6b9e546013'), 200, sample_court_json_1)
-    check(list(), 200, '['+sample_court_json_1+']')
-    check(delete('de305d54-75b4-431b-adb2-eb6b9e546013'), 200)
-    check(list(), 200, '[]')
-    check(delete('all-the-things'), 200)
-    print "done"
+    check('delete all the things', delete('all-the-things'), 200)
+    check('bad auth',              put('foo-bar', '[]', auth=False), 403)
+    check('check empty contents',  list(), 200, '[]')
+    check('missing court',         get('de305d54-75b4-431b-adb2-eb6b9e546013'), 404)
+    check('create a court',        put('de305d54-75b4-431b-adb2-eb6b9e546013', sample_court_json_1), 201)
+    check('bad json payload',      put('de305d54-75b4-431b-adb2-eb6b9e546013', 'bad json'), 400)
+    check('update a court',        put('de305d54-75b4-431b-adb2-eb6b9e546013', sample_court_json_1), 200)
+    check('check court list',      list(), 200, '['+sample_court_json_1+']')
+    check('check single court',    get('de305d54-75b4-431b-adb2-eb6b9e546013'), 200, sample_court_json_1)
+    check('delete court',          delete('de305d54-75b4-431b-adb2-eb6b9e546013'), 200)
+    check('check empty list',      list(), 200, '[]')
+    check('create court 2',        put('de305d54-75b4-431b-eb6b9e546013', sample_court_json_1), 201)
+    check('clean up',              delete('all-the-things'), 200)
+    print "done: %d errors" % num_errors
