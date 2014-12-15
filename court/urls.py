f206c0a60b93b67951306d5ca6f63cbbe55b40d7
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2014-12-15 17:57:18 +0000

    mock court pages

--- court/urls.py
+++ court/urls.py
@@ -4,4 +4,5 @@ from court import views
 urlpatterns = patterns('',
     url(r'^$', views.list),
     url(r'^(?P<uuid>[-a-z\d]+)$', views.court),
+    url(r'^public/(?P<slug>[-a-z\d]+)$', views.public),
 )
--- court/views.py
+++ court/views.py
@@ -11,7 +11,7 @@ def __failed_auth(request):
     return not hasattr(settings, 'OAUTH_TOKEN') or (request.META.get('HTTP_AUTHORIZATION','') != 'Bearer ' + settings.OAUTH_TOKEN)
 
 def __to_obj(court):
-    return {'name':court.name, 'slug':court.slug, 'number':court.number, 'updated_at':court.updated_at, 'closed': court.closed, 'alert': court.alert, 'lat':court.lat, 'lon':court.lon, 'DX':court.dx}
+    return {'uuid':court.uuid, 'name':court.name, 'slug':court.slug, 'number':court.number, 'updated_at':court.updated_at, 'closed': court.closed, 'alert': court.alert, 'lat':court.lat, 'lon':court.lon, 'DX':court.dx}
 
 def __valid_uuid(uuid):
     return re.match('\A[a-f\d]{8}-[a-f\d]{4}-[1-5][a-f\d]{3}-[89ab][a-f\d]{3}-[a-f\d]{12}\Z',uuid) or uuid == 'all-the-things'
@@ -22,6 +22,10 @@ def list(request):
     courts = Court.objects.all()
     court_objs = [__to_obj(court) for court in courts]
     return HttpResponse(json.dumps(court_objs))
+
+def public(request, slug):
+    court = get_object_or_404(Court, slug=slug)
+    return HttpResponse("<html><body><h1>Name: %s<h1><p>uuid: %s</p></body></html>" % (court.name, court.uuid))
 
 def court(request, uuid):
     if __failed_auth(request):
@@ -34,7 +38,6 @@ def court(request, uuid):
         return HttpResponse(json.dumps(court_obj))
 
     elif request.method == "PUT":
-        print request.body
         try:
             c = json.loads(request.body);
         except ValueError:
@@ -51,7 +54,7 @@ def court(request, uuid):
             court.lon=c['lon']
             court.number=c['number']
             court.dx=c['DX']
-            return HttpResponse('{"public_url":"https://www.gov.uk/prefix/%s"}' % court.slug, status=200)
+            return HttpResponse('{"public_url":"http://127.0.0.1:8000/court/public/%s"}' % court.slug, status=200)
         else:
             court = Court.objects.create(
               uuid=uuid,
@@ -65,7 +68,7 @@ def court(request, uuid):
               number=c['number'],
               dx=c['DX']
             )
-            return HttpResponse('{"public_url":"https://www.gov.uk/prefix/%s"}' % court.slug, status=201)
+            return HttpResponse('{"public_url":"https://127.0.0.1:8000/court/public/%s"}' % court.slug, status=201)
     elif request.method == "DELETE":
         if uuid == 'all-the-things':
             Court.objects.all().delete()
